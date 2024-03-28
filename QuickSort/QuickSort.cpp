38bdd76c7f9293ccb48123886450cbf300482327
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-03-28 10:12:01 +0700

    menampilkan Enter Array Element

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -19,3 +19,7 @@ void input() {
 		cout << "\nMaksimum panjang array adalah 20" << endl;
 	}
 	
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+	
